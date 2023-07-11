63a5a03703966608c4e0032e8a416c3c506f3df5
Kerem ATAM <katam@aefis.com>
2023-07-11 13:08:49 +0300

    fix fouc issues

--- src/components/bio.js
+++ src/components/bio.js
@@ -47,58 +47,56 @@ const Bio = () => {
         quality={95}
         alt="Profile picture"
       />
-      {author?.name && (
-        <p>
-          {author?.summary || null}
-          {` `}
-          <div style={{ display: "flex" }}>
-            <a href={social.github} style={socialIconStyle}>
-              <StaticImage
-                layout="fixed"
-                formats={["auto", "webp", "avif"]}
-                src="../images/github.png"
-                width={48}
-                height={48}
-                quality={95}
-                alt="Github"
-              />
-            </a>
-            <a href={social.linkedin} style={socialIconStyle}>
-              <StaticImage
-                layout="fixed"
-                formats={["auto", "webp", "avif"]}
-                src="../images/linkedin.png"
-                width={48}
-                height={48}
-                quality={95}
-                alt="Linkedin"
-              />
-            </a>
-            <a href={social.twitter} style={socialIconStyle}>
-              <StaticImage
-                layout="fixed"
-                formats={["auto", "webp", "avif"]}
-                src="../images/twitter.png"
-                width={48}
-                height={48}
-                quality={95}
-                alt="Twitter"
-              />
-            </a>
-            <a href={social.email} style={socialIconStyle}>
-              <StaticImage
-                layout="fixed"
-                formats={["auto", "webp", "avif"]}
-                src="../images/gmail.png"
-                width={48}
-                height={48}
-                quality={95}
-                alt="email"
-              />
-            </a>
-          </div>
-        </p>
-      )}
+
+      <div>
+        <p>{author.summary}</p>
+        <div style={{ display: "flex" }}>
+          <a href={social.github} style={socialIconStyle}>
+            <StaticImage
+              layout="fixed"
+              formats={["auto", "webp", "avif"]}
+              src="../images/github.png"
+              width={48}
+              height={48}
+              quality={95}
+              alt="Github"
+            />
+          </a>
+          <a href={social.linkedin} style={socialIconStyle}>
+            <StaticImage
+              layout="fixed"
+              formats={["auto", "webp", "avif"]}
+              src="../images/linkedin.png"
+              width={48}
+              height={48}
+              quality={95}
+              alt="Linkedin"
+            />
+          </a>
+          <a href={social.twitter} style={socialIconStyle}>
+            <StaticImage
+              layout="fixed"
+              formats={["auto", "webp", "avif"]}
+              src="../images/twitter.png"
+              width={48}
+              height={48}
+              quality={95}
+              alt="Twitter"
+            />
+          </a>
+          <a href={social.email} style={socialIconStyle}>
+            <StaticImage
+              layout="fixed"
+              formats={["auto", "webp", "avif"]}
+              src="../images/gmail.png"
+              width={48}
+              height={48}
+              quality={95}
+              alt="email"
+            />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
